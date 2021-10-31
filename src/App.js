4eba7c691b75da0c529a4bc4d96daef876ea1d08
Negri234279 <rafalejo@live.com>
2021-10-31 18:16:09 +0100

    conformar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+	return (
+		<div className="app">
+			<header className="app-header">
+				<h1> Calculadora de discos </h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
