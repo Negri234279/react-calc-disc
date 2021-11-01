7762014af2af42f3791d2e2c756b9d6a4d5b5b20
Negri234279 <rafalejo@live.com>
2021-11-01 23:40:29 +0100

    v1.3

--- src/components/FormDisc.js
+++ src/components/FormDisc.js
@@ -14,29 +14,43 @@ const FormDisc = () => {
         console.clear();
 
         let weightTotal = data.weightTotal - data.weightBar,
-            generateDisc = document.querySelector("#container-disc");
+            generateDisc = document.querySelector("#container-disc"),
+            numDisc25 = data.disc25,
+            numDisc20 = data.disc20,
+            numDisc15 = data.disc15,
+            numDisc10 = data.disc10,
+            numDisc5 = data.disc5,
+            numBumper = parseInt(data.bumper);
 
         generateDisc.textContent = "";
 
+        if (numBumper === 20) {
+            weightTotal -= 20;
+            generateDisc.insertAdjacentHTML("beforeend", '<div id="container-disc-b10"></div>');
+        } if (numBumper === 30) {
+            weightTotal -= 30;
+            generateDisc.insertAdjacentHTML("beforeend", '<div id="container-disc-b15"></div>');
+        }
+
         while (weightTotal > -1) {
             switch (true) {
-                case (weightTotal >= 50):
+                case (weightTotal >= 50 && numDisc25 > 0):
                     weightTotal -= 50;
                     generateDisc.insertAdjacentHTML("beforeend", '<div id="container-disc-25"></div>');
                     break;
-                case (weightTotal >= 40):
+                case (weightTotal >= 40 && numDisc20 > 0):
                     weightTotal -= 40;
                     generateDisc.insertAdjacentHTML("beforeend", '<div id="container-disc-20"></div>');
                     break;
-                case (weightTotal >= 30):
+                case (weightTotal >= 30 && numDisc15 > 0):
                     weightTotal -= 30;
                     generateDisc.insertAdjacentHTML("beforeend", '<div id="container-disc-15"></div>');
                     break;
-                case (weightTotal >= 20):
+                case (weightTotal >= 20 && numDisc10 > 0):
                     weightTotal -= 20;
                     generateDisc.insertAdjacentHTML("beforeend", '<div id="container-disc-10"></div>');
                     break;
-                case (weightTotal >= 10):
+                case (weightTotal >= 10 && numDisc5 > 0):
                     weightTotal -= 10;
                     generateDisc.insertAdjacentHTML("beforeend", '<div id="container-disc-5"></div>');
                     break;
@@ -60,12 +74,9 @@ const FormDisc = () => {
     
         <div className="formDisc">
             <form onSubmit={handleSubmit(onSubmit)}>
-                {/* Peso Barra */}
+                {/* Bar weight */}
                 <div>            
-                    <label
-                        className="form-label">
-                        Bar weight
-                    </label>
+                    <label> Bar weight </label>
                     <input
                         type="number"
                         defaultValue="20"
@@ -73,16 +84,81 @@ const FormDisc = () => {
                     />
                     {errors.weightBar && <p> This field is required </p>}
                 </div>
-                {/* Peso Total */}
+
+                {/* Total weight */}
                 <div>
-                    <label
-                        className="form-label">
-                        Total weight
-                    </label>
+                    <label> Total weight </label>
                     <input type="number" {...register("weightTotal", { required: true })} />
                     {errors.weightTotal && <p> This field is required </p>}
                 </div>
 
+                <div className="halfSelect">
+                {/* Bumper */}
+                    <div>
+                        <select {...register("bumper")}>
+                            <option value="">Bumper - 0</option>
+                            <option value="20"> Disc 10kg </option>
+                            <option value="30"> Disc 15kg </option>
+                        </select>
+                    </div>
+                
+                    {/* Disc 25 */}
+                    <div>
+                        <select {...register("disc25")}>
+                            <option value="2"> Disc's 25 - 2 </option>
+                            <option value="0"> Disc's 25 - 0 </option>
+                            <option value="1"> Disc's 25 - 1 </option>
+                            <option value="3"> Disc's 25 - 3 </option>
+                            <option value="4"> Disc's 25 - 4 </option>
+                        </select>
+                    </div>
+
+                    {/* Disc 20 */}
+                    <div>
+                        <select {...register("disc20")}>
+                            <option value="2"> Disc's 20 - 2 </option>
+                            <option value="0"> Disc's 20 - 0 </option>
+                            <option value="1"> Disc's 20 - 1 </option>
+                            <option value="3"> Disc's 20 - 3 </option>
+                            <option value="4"> Disc's 20 - 4 </option>
+                        </select>
+                    </div>
+
+                    {/* Disc 15 */}
+                    <div>
+                        <select {...register("disc15")}>
+                            <option value="2"> Disc's 15 - 2 </option>
+                            <option value="0"> Disc's 15 - 0 </option>
+                            <option value="1"> Disc's 15 - 1 </option>
+                            <option value="3"> Disc's 15 - 3 </option>
+                            <option value="4"> Disc's 15 - 4 </option>
+                        </select>
+                    </div>
+
+                    {/* Disc 10 */}
+                    <div>
+                        <select {...register("disc10")}>
+                            <option value="2"> Disc's 10 - 2 </option>
+                            <option value="0"> Disc's 10 - 0 </option>
+                            <option value="1"> Disc's 10 - 1 </option>
+                            <option value="3"> Disc's 10 - 3 </option>
+                            <option value="4"> Disc's 10 - 4 </option>
+                        </select>
+                    </div>
+
+                    {/* Disc 5 */}
+                    <div>
+                        <select {...register("disc5")}>
+                            <option value="2"> Disc's 5 - 2 </option>
+                            <option value="0"> Disc's 5 - 0 </option>
+                            <option value="1"> Disc's 5 - 1 </option>
+                            <option value="3"> Disc's 5 - 3 </option>
+                            <option value="4"> Disc's 5 - 4 </option>
+                        </select>
+                    </div>
+                </div>
+
+                {/* Submit */}
                 <div>
                     <input
                         type="submit"
