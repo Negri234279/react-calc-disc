fa8f12e97c716901918b10cc2e400c9168d0d337
Negri234279 <rafalejo@live.com>
2021-11-23 19:45:32 +0100

    refactor button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,7 @@
 import './buttom.scss';
 
 const Button = (props) => {
-    return (
-		<div>
-			<input type={props.type} value={props.value} />
-		</div>
-	);
+    return (<div> <input type={props.type} value={props.value} /> </div>);
 }
 
 export default Button
